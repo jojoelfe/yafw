49d79e7311a03226a1c26dc5072b33f4973e7d67
Johannes Elferich <jojotux123@hotmail.com>
2024-02-06 13:04:32 -0500

    Plot occupancies

--- src/yafw/cli.py
+++ src/yafw/cli.py
@@ -81,15 +81,20 @@ def create_binned_stack(
     with mrcfile.open(ctx.obj.project.stacks[0].filename, mode="r") as f:
         orig_size = f.header.nx
     bin_to = round(orig_size / bin)
+    output_filename = ctx.obj.project.path / f"{ctx.obj.project.name}B{bin}.mrc"
+    if output_filename.exists():
+        typer.echo(f"{output_filename} already exists. Exiting.")
+        raise typer.Exit(code=1)
     par = ResampleParameters(
         input_filename=str(ctx.obj.project.stacks[0].filename),
-        output_filename=str(ctx.obj.project.path / f"{ctx.obj.project.name}B{bin}.mrc"),
+        output_filename=str(output_filename),
         new_x_size=bin_to,
         new_y_size=bin_to,
     )
+    print(f"Creating binned stack {output_filename}")
     run(par)
     ctx.obj.project.stacks.append(FrealignBinnedStack(
-        filename=par.output_filename,
+        filename=output_filename,
         pixel_size_A=ctx.obj.project.stacks[0].pixel_size_A * (orig_size / bin_to)
     ))
     ctx.obj.project.save()
@@ -97,16 +102,94 @@ def create_binned_stack(
 @app.command()
 def create_job(
     ctx: Context,
+    stack: Path = typer.Argument(..., help="The stack to use"),
+    starting_parameters: str = typer.Argument("default", help="Path to the starting parameters file"),
+    parameter_mask: str = typer.Option("0 0 0 0 0", help="Path to the mask file"),
+    nclasses: int = typer.Option(10, help="Number of classes to refine"),
+    nprocessor_ref: int = typer.Option(10, help="Number of processors to use for refinement"),
+    nprocessor_rec: int = typer.Option(10, help="Number of processors to use for reconstruction"),
+    nrounds: int = typer.Option(10, help="Number of rounds to run"),
+    outer_radius: float = typer.Option(200.0, help="Outer radius of the mask"),
+    mol_mass: float = typer.Option(3000.0, help="Molecular mass of the particle"),
 ):
-    pass
+    if ctx.obj is None:
+        typer.echo("Please run in a FREALIGN project folder. Exiting.")
+        raise typer.Exit(code=1)
+    from yafw.project_management import FrealignJob, FrealignParameters
+    from shutil import copy
+    job_id = max([job.id for job in ctx.obj.project.jobs], default=0) + 1
+    job = FrealignJob(
+        id=job_id,
+        path=ctx.obj.project.path / f"job{job_id:04d}"
+    )
+    job.path.mkdir()
+    if starting_parameters == "default":
+        copy(ctx.obj.project.path / f"{ctx.obj.project.name}.par", job.path / f"{ctx.obj.project.name}_1_r1.par")
+    else:
+        copy(starting_parameters, job.path / f"{ctx.obj.project.name}_1_r1.par")
+    stack_info = list(filter(lambda x: x.filename.absolute() == stack.absolute(),ctx.obj.project.stacks))[0]
+    
+    parameters = FrealignParameters(
+        parameter_mask=parameter_mask,
+        nclasses=nclasses,
+        nprocessor_ref=nprocessor_ref,
+        nprocessor_rec=nprocessor_rec,
+        start_process=2,
+        end_process=2 + nrounds - 1,
+        data_input=ctx.obj.project.name,
+        raw_images=str(stack.absolute()),
+        outer_radius=outer_radius,
+        mol_mass=mol_mass,
+        pix_size = stack_info.pixel_size_A,
+        dstep=(ctx.obj.project.detector_pixelsize_A * (stack_info.pixel_size_A / ctx.obj.project.original_pixelsize_A))/10000,
+    )
+    parameters.render(job.path / "mparameters")
+    ctx.obj.project.jobs.append(job)
+    ctx.obj.project.save()
+
+@app.command()
+def job_status(ctx: Context):
+    from yafw.frealign_jobs import parse_job
+    if ctx.obj is None or ctx.obj.job is None:
+        typer.echo("Please run in a FREALIGN job folder. Exiting.")
+        raise typer.Exit(code=1)
+    job_data = parse_job(ctx.obj.project, ctx.obj.job)
+
+    import matplotlib.pyplot as plt
+    for class_n in range(job_data[-1][-1].class_n):
+        data = job_data[class_n]
+        plt.plot([d.round for d in data], [d.avg_occ for d in data], label=f"Class {class_n+1}")
+    plt.legend()
+    plt.show()
 
 @app.callback()
 def main(
     ctx: Context,
 ):
-    from yafw.project_management import FrealignProject
+    from yafw.project_management import FrealignProject, FrealignJob
     if (Path.cwd() / f"{Path.cwd().name}.json").exists():
-        ctx.obj = Global(project=FrealignProject.open(f"{Path.cwd() / Path.cwd().name}.json"))
+        ctx.obj = Global(project=FrealignProject.open(f"{Path.cwd() / Path.cwd().name}.json"),job=None)
+    elif (Path.cwd().parent / f"{Path.cwd().parent.name}.json").exists():
+        project = FrealignProject.open(f"{Path.cwd().parent / Path.cwd().parent.name}.json")
+        job = list(filter(lambda x: x.path.absolute() == Path.cwd().absolute(),project.jobs))
+        if len(job) == 1:
+            job = job[0]
+        else:
+            job = FrealignJob(
+                id=0,
+                path=Path.cwd()
+            )
+        ctx.obj = Global(project=project,
+                         job=job)
+    else:
+        project = None
+        job = FrealignJob(
+                id=0,
+                path=Path.cwd()
+            )
+        ctx.obj = Global(project=project,
+                         job=job)
+
 
 if __name__ == "__main__":
     app()
--- src/yafw/project_management.py
+++ src/yafw/project_management.py
@@ -41,8 +41,101 @@ class FrealignProject(BaseModel):
         return cls.model_validate_json(data)
 
 class Global(BaseModel):
-    project: FrealignProject
+    project: Optional[FrealignProject]
+    job: Optional[FrealignJob]
     
 
 class Context(typer.Context):
     obj: Optional[Global] = None
+
+class FrealignParameters(BaseModel):
+    cluster_type: str = "none"
+    nprocessor_ref: int = 10
+    nprocessor_rec: int = 10
+    mem_per_cpu: int = 2048
+    MODE: int = 1
+    start_process: int = 2
+    end_process: int = 10
+    res_high_refinement: float = 16.0
+    res_high_class: float = 16.0
+    nclasses: int = 10
+    data_input: str = "stack"
+    raw_images: str = "stack.mrc"
+    image_contrast: str = "N"
+    outer_radius: float = 200.0
+    inner_radius: float = 0.0
+    mol_mass: float = 1000
+    Symmetry: str = "C1"
+    pix_size: float = 8.0
+    dstep: float = 37.7
+    Aberration: float = 2.7
+    Voltage: float = 300.0
+    Amp_contrast: float = 0.07
+    mask_file: str = ""
+    mask_edge: int = 5
+    mask_outside_weight: float = 0.0
+    mask_filt_res: float = 0.0
+    mask_filt_edge: int = 5
+    focus_mask: str = ""
+    DANG: float = 0.0
+    parameter_mask: str = "0 0 0 0 0"
+    refineangleinc: int = 4
+    refineshiftinc: int = 4
+    res_reconstruction: float = 0.0
+    res_low_refinement: float = 50.0
+    thresh_reconst: float = 0.0
+    nbootstrap: int = 1000
+    FDEF: str = 'F'
+    FMATCH: str = 'F'
+    FBOOST: str = 'T'
+    mp_cpus: int = 1
+    restart_after_crash: str = 'F'
+    delete_scratch: str = 'T'
+    qsub_string_ref: str = ""
+    qsub_string_rec: str = ""
+    first_particle: str = ""
+    last_particle: str = ""
+    frealign_bin_dir: str = ""
+    scratch_dir: str = ""
+    ITMAX: int = 200
+    XSTD: float = 0.0
+    PBC: float = 2.0
+    thresh_refine: float = 50.0
+    RBfactor: float = 0.0
+    FMAG: str = 'F'
+    FASTIG: str = 'F'
+    FPART: str = 'F'
+    FFILT: str = 'T'
+    FBEAUT: str = 'F'
+    beam_tilt_x: float = 0.0
+    beam_tilt_y: float = 0.0
+    BSC: float = 2.0
+    percentage: float = 1.0
+    normalize_images: str = 'F'
+    crop_images: str = 'F'
+    adjust_scores: str = 'T'
+    search_mask_radius: float = 0.0
+    search_range_x: float = 0.0
+    search_range_y: float = 0.0
+    boost_limit: float = 0.0
+    P3Dblurring: str = 'F'
+    even_odd_fsc: str = 'T'
+
+    def render(self, filename):
+        with open(filename, "w") as f:
+            for k, v in self.model_dump().items():
+                if k in ["parameter_mask", "focus_mask"]:
+                    f.write(f'{k}\t\t"{v}"\n')
+                else:
+                    f.write(f'{k}\t\t{v}\n')
+    
+    @classmethod
+    def open(cls, filename):
+        with open(filename, "r") as f:
+            data = f.readlines()
+        out = cls()
+        for line in data:
+            k, v, *_ = line.split("")
+            setattr(out, k, v)
+        return out
+            
